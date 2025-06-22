f288768bbf1161ad53df1ea7517e33fe6b73aae1
Chili Chonka <7570582+ChiliChonka@users.noreply.github.com>
2025-06-22 15:13:32 +0200

    Use pnet for private cluster

--- orbitdb-node/src/config/libp2p.ts
+++ orbitdb-node/src/config/libp2p.ts
@@ -1,15 +1,12 @@
 import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import { identify } from '@libp2p/identify'
+import { preSharedKey } from '@libp2p/pnet'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
-import { mdns } from '@libp2p/mdns'
 
 export const Libp2pOptions = {
-  peerDiscovery: [
-    mdns()
-  ],
   addresses: {
     listen: ['/ip4/0.0.0.0/tcp/0', '/ip4/0.0.0.0/tcp/0/ws']
   },
@@ -17,6 +14,7 @@ export const Libp2pOptions = {
     tcp(),
     webSockets()
   ],
+  connectionProtector: preSharedKey({ psk: new TextEncoder().encode('/key/swarm/psk/1.0.0/\n/base16/\nfef2d1aa529dfa67806cd9b7e8984c5c38425cbda4fd3ec208ef4b0f78194844\n') }),
   connectionEncrypters: [noise()],
   streamMuxers: [yamux()],
   services: {
--- orbitdb-webapp/src/main.ts
+++ orbitdb-webapp/src/main.ts
@@ -1,11 +1,17 @@
 import { createLibp2p } from 'libp2p'
 import { createHelia } from 'helia'
 import { webSockets } from '@libp2p/websockets'
+import { preSharedKey } from '@libp2p/pnet'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { identify } from '@libp2p/identify'
 import { multiaddr } from '@multiformats/multiaddr'
+  const swarmKey = new TextEncoder().encode(
+    '/key/swarm/psk/1.0.0/\n/base16/\nfef2d1aa529dfa67806cd9b7e8984c5c38425cbda4fd3ec208ef4b0f78194844\n'
+  )
+
+    connectionProtector: preSharedKey({ psk: swarmKey }),
       identify: identify(),
   const peerAddr = new URLSearchParams(window.location.search).get('peer')
   if (peerAddr) {
